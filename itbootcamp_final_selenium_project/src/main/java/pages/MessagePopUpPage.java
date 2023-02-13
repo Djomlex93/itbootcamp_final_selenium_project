1268ed6755b5feda2e975265c3ef2417c53cbac2
Mladen Dimitrijevic <mladendimitrijevic93@gmail.com>
2023-02-13 18:19:36 +0100

    Adding MessagePopUpPage

---
+++ itbootcamp_final_selenium_project/src/main/java/pages/MessagePopUpPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MessagePopUpPage extends BasePage{
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+    public void waitUntilMessagePopUpIsVisible(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div/div[1]/ul/li")));
+    }
+
+    public String popUpText() {
+        return driver.findElement(By.xpath("//div/div[1]/ul/li")).getText();
+    }
+    public WebElement getCloseButton() {
+        return driver.findElement(By.xpath("//div[1]/button"));
+    }
+    public void waitForVerifyYourAccountPopUp() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.className("dlgVerifyAccount")));
+    }
+    public String verifyPopUpDialogueText() {
+        return driver.findElement(By.className("dlgVerifyAccount")).getText();
+    }
+    public WebElement getCloseButtonVerifyPopUp() {
+        return driver.findElement(By.xpath("//div/div/div[3]/button"));
+    }
+}
