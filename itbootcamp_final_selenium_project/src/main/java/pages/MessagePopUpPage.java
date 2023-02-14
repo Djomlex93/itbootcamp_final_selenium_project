e945e66dac1842f0f368e32428c4ab38d85536cd
Mladen Dimitrijevic <mladendimitrijevic93@gmail.com>
2023-02-14 16:28:36 +0100

    Updated MessagePopUpPage and AdminCitiesTests

--- itbootcamp_final_selenium_project/src/main/java/pages/MessagePopUpPage.java
+++ itbootcamp_final_selenium_project/src/main/java/pages/MessagePopUpPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class MessagePopUpPage extends BasePage{
     public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -30,5 +32,14 @@ public class MessagePopUpPage extends BasePage{
     }
     public WebElement getCloseButtonVerifyPopUp() {
         return driver.findElement(By.xpath("//div/div/div[3]/button"));
+    }
+    public void waitForPopUpSavedOrDeleted() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div[2]/div/div[3]/div/div/div/div/div[1]")));
+    }
+
+    public String popUpSavedOrDeletedText() {
+        return driver.findElement
+                (By.xpath("//div[2]/div/div[3]/div/div/div/div/div[1]")).getText();
     }
 }
--- itbootcamp_final_selenium_project/src/test/java/tests/AdminCitiesTests.java
+++ itbootcamp_final_selenium_project/src/test/java/tests/AdminCitiesTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.Keys;
 import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -38,7 +39,55 @@ public class AdminCitiesTests extends BasicTest {
         citiesPage.waitForEditAndCreateDialogueToBeVisible();
         citiesPage.getInputName().sendKeys("Mladen Dimitrijevic's city");
         citiesPage.getSaveButtonFromDialogue().click();
-       //fali popup
+        messagePopUpPage.waitForPopUpSavedOrDeleted();
+        Assert.assertTrue(messagePopUpPage.popUpSavedOrDeletedText().contains("Saved successfully"),
+                "Proper message is not displayed, it should contain 'Saved successfully'.");
+
+    }
+    @Test(priority = 40)
+    @Description("Edit city")
+    public void editCity(){
+        navPage.getAdminButton().click();
+        navPage.getCitiesLink().click();
+        citiesPage.getSearchInput().sendKeys("Mladen Dimitrijevic's city");
+        citiesPage.waitUntilRowsAppears(1);
+        citiesPage.getEditButtonFromRow(1).click();
+        citiesPage.getInputName().click();
+        citiesPage.getInputName().sendKeys(Keys.CONTROL + "a");
+        citiesPage.getInputName().sendKeys(Keys.BACK_SPACE);
+        citiesPage.getInputName().sendKeys("Mladen Dimitrijevic's city Edited");
+        citiesPage.getSaveButtonFromDialogue().click();
+        messagePopUpPage.waitForPopUpSavedOrDeleted();
+        Assert.assertTrue(messagePopUpPage.popUpSavedOrDeletedText().contains("Saved successfully"),
+                "Proper message is not displayed, it should contain 'Saved successfully'.");
+
+    }
+    @Test(priority = 50)
+    @Description("Search city")
+    public void searchCity(){
+        navPage.getAdminButton().click();
+        navPage.getCitiesLink().click();
+        citiesPage.getSearchInput().sendKeys("Mladen Dimitrijevic's city Edited");
+        citiesPage.waitUntilRowsAppears(1);
+        Assert.assertTrue(citiesPage.getCellByColumnAndRow(1, 2).getText().contains("Mladen Dimitrijevic's city Edited"),
+                "Wrong city name.");
+    }
+    @Test(priority = 60)
+    @Description("Delete city")
+    public void deleteCity() throws InterruptedException {
+        navPage.getAdminButton().click();
+        navPage.getCitiesLink().click();
+        citiesPage.getSearchInput().sendKeys("Mladen Dimitrijevic's city Edited");
+        citiesPage.waitUntilRowsAppears(1);
+        Assert.assertTrue(citiesPage.getCellByColumnAndRow(1, 2).getText().contains("Mladen Dimitrijevic's city Edited"),
+                "Wrong city name.");
+        citiesPage.getDeleteButtonFromRow(1).click();
+        Thread.sleep(3000);
+        citiesPage.waitForDeleteDialogueToBeVisible();
+        citiesPage.getDeleteButtonFromDialogue().click();
+        messagePopUpPage.waitForPopUpSavedOrDeleted();
+        Assert.assertTrue(messagePopUpPage.popUpSavedOrDeletedText().contains("Deleted successfully"),
+                "Proper message is not displayed, it should contain 'Deleted successfully'.");
 
 
     }
